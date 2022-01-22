0223cc06d72cdfc6d816a453776a6eda151b4bcb
Rami-Suwayyed <swead96@gmail.com>
2022-01-22 23:24:25 +0200

    app1_email_verification

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
